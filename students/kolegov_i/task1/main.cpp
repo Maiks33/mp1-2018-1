5173854710fd66ef3d498efbb974bdb484ed5d0e
Elijah <Superil2q@yandex.ru>
2018-02-23 14:47:48 +0300

    Объявления класса, его полей и методов

--- students/kolegov_i/task1/main.cpp
+++ students/kolegov_i/task1/main.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
+class LengthConverter
+{
+private:
+	double meters;
+public:
+	LengthConverter(int m); 
+
+	//Мне показалась такая реализация наиболее интересной
+	//Возможно как-то можно использовать enum
+	const int FOOT = 1;
+	const int YARD = 2;
+	const int ARSHIN = 3;
+	const int SAZHEN = 4;
+	double ConvertTo(int MeraType);
+
+	double GetLenght();
+	void SetLength(double m);
+	
+	void ToConsole();
+
+
+
+};
+
 int main()
 {
   cout << "Hello!" << endl;
