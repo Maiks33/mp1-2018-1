31977e807eeeac26a0549f6720bedb4c9d21c6f6
Elijah <Superil2q@yandex.ru>
2018-02-23 15:12:29 +0300

    Добавлена реализация

--- students/kolegov_i/task1/main.cpp
+++ students/kolegov_i/task1/main.cpp
@@ -9,6 +9,21 @@ private:
 public:
 	LengthConverter(int m); 
 
+	LengthConverter& operator=(const LengthConverter& lc)
+	{
+		meters = lc.meters;
+		return *this;
+	}
+
+	LengthConverter& operator=(const double& a)
+	{
+		if (a >= 0)
+			meters = a;
+		else
+			meters = -1;
+		return *this;
+	}
+
 	//Мне показалась такая реализация наиболее интересной
 	//Возможно как-то можно использовать enum
 	const int FOOT = 1;
@@ -21,13 +36,76 @@ public:
 	void SetLength(double m);
 	
 	void ToConsole();
-
-
-
 };
+
+
+LengthConverter::LengthConverter(int m)
+{
+	if (m >= 0)
+		meters = m;
+	else
+		meters = -1;
+}
+
+double LengthConverter::GetLenght()
+{
+	return meters;
+}
+
+void LengthConverter::SetLength(double m)
+{
+	if (m >= 0)
+		meters = m;
+	else
+		meters = -1;
+}
+
+double LengthConverter::ConvertTo(int param)
+{
+	switch (param)
+	{
+	default: return -1;
+		case 1: return meters * 3.28084;
+		case 2: return meters * 1.09361;
+		case 3: return meters * 1.4060742407;
+		case 4: return meters * 0, 546807;
+	}
+
+}
+
+void LengthConverter::ToConsole()
+{
+	cout << "\tMeters: " << ConvertTo(FOOT);
+	cout << "\n\tYard: " << ConvertTo(YARD);
+	cout << "\n\tArshin: " << ConvertTo(ARSHIN);
+	cout << "\n\tSazhen: " << ConvertTo(SAZHEN);
+}
 
 int main()
 {
-  cout << "Hello!" << endl;
-  return 0;
+	LengthConverter a(5);
+	a = 15;
+	LengthConverter b(-8);
+	b = a;
+
+	double i[5];
+	i[0] = b.ConvertTo(b.ARSHIN);
+	i[1] = b.ConvertTo(b.YARD);
+	i[2] = b.ConvertTo(b.SAZHEN);
+	i[3] = b.ConvertTo(b.FOOT);
+	i[4] = b.GetLenght();
+
+	cout << "Object b:\n{\n";
+	for (int j = 0; j < 5; j++)
+	{
+		cout <<"\t"<< i[j] << "\n";
+	}
+
+	cout << "}\nObject a:\n{\n";
+	a.ToConsole();
+
+	cout << "\n}\n";
+	system("pause");
+
+	return 0;
 }
