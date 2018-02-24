7dfee451ff729c602b5748961eea6709c3133de0
Anilmal <baranovart231999@gmail.com>
2018-02-24 21:44:56 +0300

    Новая программа

--- students/Baranov_A/task1/main.cpp
+++ students/Baranov_A/task1/main.cpp
@@ -1,6 +1,6 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #include <cstdlib>
 
 using namespace std;
@@ -8,7 +8,7 @@ using namespace std;
 void Vlogenie(int &hor, int &min, int &sec)//ввод данных
 {
 	cout << "Введите ЧЧ : ММ: СС " << endl;
-	cin >> hor;cout << ":";cin >> min;cout << ":";cin >> sec;
+	cin >> hor;cin >> min;cin >> sec;
 }
 
 
@@ -16,40 +16,20 @@ class Time
 {
 private:
 	int hor, min, sec;
-	void time()//оптимизация переменных
-	{
-		if (sec > 59)
-		{
-			min = min + sec / 60;
-			sec = sec % 60;
-		}
-		if (min > 59)
-		{
-			hor = hor + min / 60;
-			min = min % 60;
-		}
-		if (hor > 23)
-		{
-			hor = hor % 24;
-		}
-	}
+
 public:
 	//конструктор
-	Time(int _hor, int _min, int _sec)
-	{
-		setTime(_hor, _min, _sec);
-	}
-	//Вывод на экран
-	void ShowTime()
-	{
-		cout << hor << " : " << min << " : " << sec << endl;
-	}
-	//Установка времени
+	Time(int _hor = 0, int _min = 0, int _sec = 0) :hor(_hor), min(_min), sec(_sec) {}
 	void setTime(int _hor, int _min, int _sec)
 	{
 		hor = _hor;
 		min = _min;
 		sec = _sec;
+	}
+	//Вывод на экран
+	void ShowTime()
+	{
+		cout << hor << " : " << min << " : " << sec << endl;
 	}
 	//Оператор присваивания
 	Time operator = (const Time& obj)
@@ -59,41 +39,61 @@ public:
 		sec = obj.sec;
 		return *this;
 	}
-	//сдвиг времени в меньшую сторону
-	Time operator - (const Time& t2)
+	//Сдвиг времени 
+	void Change(int _hor, int _min, int _sec, int change)
 	{
-		int sec_ = sec - t2.sec;
-		int min_ = min - t2.min;
-		int hor_ = hor - t2.hor;
-		if (sec_ < 0)
-			sec_ = 60 - sec_ % 60;
-		min_ = min_ - (1 + abs(sec / 60));
-		if (min_ < 0)
-			min_ = 60 - min_ % 60;
-		hor_ = hor_ - (1 + abs(min / 60));
-		if (hor_ < 0)
-			hor_ = 24 - hor_ % 24;
-	}
-	//Сдвиг времени в большую сторону
-	Time operator + (const Time& t2)
-	{
-		int sec_ = sec + t2.sec;
-		int min_ = min + t2.min;
-		int hor_ = hor + t2.hor;
+		if (change == 1)
+		{
+			hor += _hor;
+			min += _min;
+			sec += _sec;
+		}
+		if (change == 2)
+		{
+			hor -= _hor;
+			min -= _min;
+			sec -= _sec;
+		}
 		if (sec > 59)
 		{
 			min = min + sec / 60;
 			sec = sec % 60;
+		}
+		if (sec < 0)
+		{
+			min = min - (1 + abs(_sec / 60));
+			sec = 60 - sec % 60;
 		}
 		if (min > 59)
 		{
 			hor = hor + min / 60;
 			min = min % 60;
 		}
-		if (hor > 23)
+		if (min < 0)
 		{
-			hor = hor % 24;
+			hor = hor - (1 + abs(min / 60));
+			min = 60 - min % 60;
 		}
+		if (hor > 23)
+			hor = hor % 24;
+		if (hor == -24)
+			hor = 0;
+		if (hor < 0)
+			hor = 24 + hor % 24;
+	}
+	//Разница
+	void Raznica(int hor2, int min2, int sec2)
+	{
+		int Secfirst;
+		int Secsecond;
+		int Razn;
+		Secfirst = hor * 3600 + min * 60 + sec;
+		Secsecond = hor2 * 3600 + min2 * 60 + sec2;
+		Razn = abs(Secfirst - Secsecond);
+		hor = Razn / 3600;
+		Razn = Razn - hor * 3600;
+		min = Razn / 60;
+		sec = Razn % 60;
 	}
 };
 int main()
@@ -103,8 +103,7 @@ int main()
 	int min = 0;
 	int sec = 0;
 	Time T;
-	Time time2;
-	Time time3;
+	system("chcp 1251>nul");
 	cout << "Желаете задать время?" << endl;
 in:		cout << "1-Да,2-Нет" << endl;
 	cin >> choice;
@@ -113,11 +112,9 @@ in:		cout << "1-Да,2-Нет" << endl;
 		Vlogenie(hor, min, sec);
 		T.setTime(hor, min, sec);
 		T.ShowTime();
-		cout << T.ShowTime() << endl;
 		system("pause");
-		system("cls");
 	}
-	if (choice != 0)
+	if (choice != 1)
 		goto in;
 	choice = 0;
 	cout << "Какие операции хотите сделать" << endl;
@@ -127,26 +124,18 @@ in:		cout << "1-Да,2-Нет" << endl;
 	{
 		cout << "В большую сторону-1,в меньшую сторону-2" << endl;
 		cin >> change;
-		if (change == 1)
-			Vlogenie(hor, min, sec);
-		time2.setTime(hor, min, sec);
-		T = T + time2;
+		Vlogenie(hor, min, sec);
+		T.Change(hor, min, sec, change);
 		T.ShowTime();
-		cout << T.ShowTime << endl;
-		if (change == 2)
-			Vlogenie(hor, min, sec);
-		time2.setTime(hor, min, sec);
-		T = T - time2;
-		T.ShowTime();
-		cout << T.ShowTime << endl;
+		system("pause");
 	}
 	if (choice == 2)
-		//Разница будет операций в главной функции,а не методом класса
+	{
 		Vlogenie(hor, min, sec);
-	time2.setTime(hor, min, sec);
-	time3 = T - time2;
-	time3.ShowTime();
-	cout << time3.ShowTime << endl;
+		T.Raznica(hor, min, sec);
+		T.ShowTime();
+		system("pause");
+	}
 	if (choice == 3)
 		return 0;
 }
